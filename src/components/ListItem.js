ebdcf277435d540f9d5f1bc437a78965917dd95c
alimkin.e@gmail.com <out_alimkin.em@pecom.ru>
2025-02-09 21:05:53 +0300

    first commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,22 @@
+import { Component } from '../core/Component'
+
+export class ListItem extends Component {
+  setup(props) {
+    this.state = {
+      id: Date.now(),
+      date: new Date().toLocaleDateString('ru-RU', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+      }),
+      amount: props.amount,
+    }
+    this.$rootElement = document.createElement('div')
+    this.$rootElement.className = 'donate-item'
+    this.$rootElement.textContent = `${this.state.date} -`
+
+    const $b = document.createElement('b')
+    $b.textContent = ` $${this.state.amount}`
+    this.$rootElement.append($b)
+  }
+}
